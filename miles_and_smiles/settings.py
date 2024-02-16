661e5b717203911aada78b352514feedc556908d
Yousefalm1 <yousefalm@outlook.com>
2024-02-16 20:24:15 +0300

    fixed heorku

--- miles_and_smiles/settings.py
+++ miles_and_smiles/settings.py
@@ -2,6 +2,8 @@
 
 import os
 from pathlib import Path
+import dj_database_url
+
 from dotenv import load_dotenv
 
 
@@ -103,12 +105,19 @@ WSGI_APPLICATION = 'miles_and_smiles.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+
 
 
 # Password validation
