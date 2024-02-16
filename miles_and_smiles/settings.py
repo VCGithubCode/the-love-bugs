f740db21fa02107425f628a3c597bbf8b2921fe5
Yousefalm1 <yousefalm@outlook.com>
2024-02-16 20:34:22 +0300

    fix allowedhost

--- miles_and_smiles/settings.py
+++ miles_and_smiles/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', 'miles-and-smiles-03ded9dd0f57.herokuapp.com/']
+ALLOWED_HOSTS = ['*', 'miles-and-smiles-03ded9dd0f57.herokuapp.com']
 
 
 # Application definition
