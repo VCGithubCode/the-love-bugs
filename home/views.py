a4f260b79deaf1b7084a75f624b54bf5ae240f37
Yousefalm1 <yousefalm@outlook.com>
2024-02-16 18:10:18 +0300

    Add base nav bar and created profile view

--- home/views.py
+++ home/views.py
@@ -5,3 +5,10 @@ def index(request):
     """ A view to return the index page """    
     
     return render(request, 'home/index.html')
+
+
+
+def profile(request):
+    """ A view to return the users profile """
+
+    return render(request, 'home/profile.html')
