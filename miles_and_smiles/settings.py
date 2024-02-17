b7a1b0cc95d93a6876ac07131346b2bf3af32be3
michmattera <michellemattera97@gmail.com>
2024-02-17 11:22:21 +0000

    Minor change

--- miles_and_smiles/settings.py
+++ miles_and_smiles/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '8000-vcgithubcod-thelovebugs-q43umjyfc9q.ws-eu108.gitpod.io', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '*', '8000-vcgithubcod-thelovebugs-q43umjyfc9q.ws-eu108.gitpod.io', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://8000-vcgithubcod-thelovebugs-6e57bg4aiio.ws-eu108.gitpod.io']
 
 # CSRF_TRUSTED_ORIGINS = ['https://8000-vcgithubcod-thelovebugs-qmbdp8tkgn4.ws-eu108.gitpod.io'] # using locally mm/ do not touch
 # Application definition
