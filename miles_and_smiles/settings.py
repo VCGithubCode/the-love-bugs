562a00d9ab07966e85754b9fe6c934f90beb7606
Yousefalm1 <yousefalm@outlook.com>
2024-02-19 10:54:11 +0300

    Static files for heroku

--- miles_and_smiles/settings.py
+++ miles_and_smiles/settings.py
@@ -116,6 +116,8 @@ USE_L10N = True
 USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS =(os.path.join(BASE_DIR, 'static'),)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
